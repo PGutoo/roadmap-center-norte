9ca96b293170e58f35870a063200af49a131ec65
Pedro <pedro.link25@gmail.com>
2024-07-12 23:49:31 -0300

    FEAT: Incluindo fastapi

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ from integration.client_integration import get_customers_by_email
 app = FastAPI()
 
 
-@app.get("/login/{int:email}")
-def login_cliente(email: int):
+@app.get('/login/{email}')
+def login_cliente(email):
     try:
         # Agora você pode acessar os dados como um objeto JSON
-        # logging.info("Verificando cliente no Clube")
+        logging.info("Verificando cliente no Clube")
         # response = get_customers_by_email(email)
         return {
-            "statusCode": 200,
-            "body": "Hello, World!"
+            'statusCode': 200,
+            'body': email
         }
     except Exception as e:
         print(e)
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
 # @app.route('/roadmap', methods=['GET'])
 # def lambda_handler():
 #     try:
