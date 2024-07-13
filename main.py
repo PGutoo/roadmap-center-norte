7357bce86254236c017d63b72e4508da7211e290
Pedro <pedro.link25@gmail.com>
2024-07-12 21:53:48 -0300

    FIX: Ajuste acesso path parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from integration.client_integration import get_customers_by_email
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/login/<email>', methods=['GET'])
 def lambda_handler(email: str):
     try:
-        response = get_customers_by_email(email)
+        response = get_customers_by_email(request.view_args['email'])
         return response
     except Exception as e:
         print(e)
