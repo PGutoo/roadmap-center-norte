27880ebc19a552beeb497ea127dfd843cef0c9dc
Pedro <pedro.link25@gmail.com>
2024-07-12 23:33:02 -0300

    FEAT: Incluindo fastapi

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from integration.client_integration import get_customers_by_email
 app = FastAPI()
 
 
-@app.get("/login/{email}")
+@app.get("/login/{int:email}")
 def login_cliente(email: int):
     try:
         # Agora você pode acessar os dados como um objeto JSON
