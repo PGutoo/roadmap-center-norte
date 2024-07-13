75ed4ed11b25a6c2891960b7a981680378656372
Pedro <pedro.link25@gmail.com>
2024-07-12 22:29:40 -0300

    FIX: Correção path parameter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import logging
-import json
 from flask import Flask, request
 from integration.client_integration import get_customers_by_email
 
@@ -9,13 +8,10 @@ app = Flask(__name__)
 @app.route('/login/<email>', methods=['GET'])
 def login_cliente(email):
     try:
-        data = request.data.decode('utf-8')
-        json_data = json.loads(data)
-
+        data = request.args.get('email')
         # Agora você pode acessar os dados como um objeto JSON
-        email = json_data.get('email')
         logging.info("Verificando cliente no Clube")
-        response = get_customers_by_email(email)
+        response = get_customers_by_email(data)
         return response
     except Exception as e:
         print(e)
