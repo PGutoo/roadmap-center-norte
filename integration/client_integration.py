3d845de8aa5ba5c26df877b6224a2acf5fa2c8c6
Pedro <pedro.link25@gmail.com>
2024-07-13 00:43:47 -0300

    FIX: Correção retorno

--- integration/client_integration.py
+++ integration/client_integration.py
@@ -6,15 +6,16 @@ class Integration:
 
     def get_customers_by_email(self, email):
         url = f"{os.environ.get('API_URL')}/customers/details"
-        listagem_clientes = requests.get(url)
-        for cliente in listagem_clientes:
-            if email == cliente.get('email'):
-                return {
-                    'nome': cliente.get('firstName'),
-                    'preferencias': cliente.get('preferences')
-                }
-            else:
-                return {
-                    'statusCode': 400,
-                    'body': 'Cliente não encontrado'
-                }
+        return url
+        # listagem_clientes = requests.get(url)
+        # for cliente in listagem_clientes:
+        #     if email == cliente.get('email'):
+        #         return {
+        #             'nome': cliente.get('firstName'),
+        #             'preferencias': cliente.get('preferences')
+        #         }
+        #     else:
+        #         return {
+        #             'statusCode': 400,
+        #             'body': 'Cliente não encontrado'
+        #         }
