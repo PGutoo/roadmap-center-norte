9b4bc78e569e9db0fc1d4b884f77dc648e5949f4
Pedro <pedro.link25@gmail.com>
2024-07-13 00:29:57 -0300

    FEAT: Incluindo fastapi

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ from integration.client_integration import get_customers_by_email
 app = Flask(__name__)
 
 
-@app.route('/login', methods=['GET'])
-def login_cliente():
+@app.route('/login/<email>', methods=['GET'])
+def login_cliente(email):
     try:
         # Agora você pode acessar os dados como um objeto JSON
-        logging.info("Verificando cliente no Clube")
         # response = get_customers_by_email(data)
         return {
             'statusCode': 200,
-            'body': "Hello World"
+            'body': email
         }
     except Exception as e:
         print(e)
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
 # @app.route('/roadmap', methods=['GET'])
 # def lambda_handler():
 #     try:
