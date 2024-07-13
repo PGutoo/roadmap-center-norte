5bf1507d50ba9509aad1fb3f9a50dd0b8d171326
Pedro <pedro.link25@gmail.com>
2024-07-12 21:47:00 -0300

    FEAT: Validação de cliente e listagem de preferencias

--- integration/client_integration.py
+++ integration/client_integration.py
@@ -2,6 +2,17 @@ import requests
 import os
 
 
-def get_customers_by_email():
+def get_customers_by_email(email: str):
     url = f"{os.environ.get('API_URL')}/customers/details"
-    return url
+    listagem_clientes = requests.get(url)
+    for cliente in listagem_clientes:
+        if email == cliente.get('email'):
+            return {
+                'nome': cliente.get('firstName'),
+                'preferencias': cliente.get('preferences')
+            }
+        else:
+            return {
+                'statusCode': 400,
+                'body': 'Cliente não encontrado'
+            }
--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from integration.client_integration import get_customers_by_email
 app = Flask(__name__)
 
 
-@app.route('/roadmap', methods=['GET'])
-def lambda_handler():
+@app.route('/login/<email>', methods=['GET'])
+def lambda_handler(email: str):
     try:
-        """
-        TODO
-        Determinar         
-        """
-        return get_customers_by_email()
+        response = get_customers_by_email(email)
+        return response
     except Exception as e:
         print(e)
+
+
+# @app.route('/roadmap', methods=['GET'])
+# def lambda_handler():
+#     try:
+#         """
+#         TODO
+#         Determinar
+#         """
+#         return get_customers_by_email()
+#     except Exception as e:
+#         print(e)
