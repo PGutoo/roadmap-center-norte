bd6516e9f30b099455e24c36ac75cb1e43f0bb5a
Pedro <pedro.link25@gmail.com>
2024-10-22 20:00:27 -0300

    FEAT: Listando lojas por preferencia

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from integration.client_integration import get_customers_by_email, get_store_by_
 app = Flask(__name__)
 
 
+@app.route('/status', methods=['GET'])
+def status():
+    return 'OK'
+
+
 @app.route('/login/<email>', methods=['GET'])
 def login_cliente(email):
     try:
@@ -29,3 +34,7 @@ def cashback(customer_id):
         return response
     except Exception as e:
         print(e)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
